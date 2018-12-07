081ba91fb029b65adf075c745b92b55d4d3ec2a1
Andrey Golubyatnikov <andre213av@gmail.com>
2018-12-07 09:03:12 +0300

    'some'

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var form = document.querySelector('#form')
+	form.addEventListener('submit', function(e) {
+		var fields = this.querySelectorAll('.req');
+	});
+});
