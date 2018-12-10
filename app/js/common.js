be2e734c2477201b4259a35972e14bb623b7ba18
lofi4ever <gai@one-touch.ru>
2018-12-10 13:06:04 +0300

    js

--- app/js/common.js
+++ app/js/common.js
@@ -26,4 +26,45 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 		if(!isValid) e.preventDefault();
 	});
+
+	var toggleBlocks = document.querySelectorAll('.js-toggle');
+	if(toggleBlocks.length) {
+		[].forEach.call(toggleBlocks, function(block) {
+			block.addEventListener('click', function(e) {
+				e.preventDefault();
+				var classList = e.target.classList;
+				if(!classList.contains('js-toggle-btn')) return
+				if(e.target.nodeType != 1) return;
+				var content = this.querySelector('.js-toggle-content'),
+						btns = this.querySelectorAll('.js-toggle-btn');
+				if(classList.contains('js-toggle-show')) {
+					content.classList.remove('hidden');
+				} else if(classList.contains('js-toggle-hide')) {
+					content.classList.add('hidden');
+				}
+				[].forEach.call(btns, function(btn) {
+					btn.classList.toggle('hidden');
+				});
+			});
+		});
+	}
+
+	var scrollBtns = document.querySelectorAll('.js-scroll');
+	var scrollTarget = document.getElementById('form-block');
+	if(scrollBtns.length) {
+		[].forEach.call(scrollBtns, function(btn) {
+			btn.addEventListener('click', function() {
+				scrollTo(scrollTarget);
+			});
+		});
+	}
+
 });
+
+function scrollTo(element) {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: element.getBoundingClientRect().top + window.scrollY
+  });
+}
